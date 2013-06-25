132c540c9ed5c74f1cb9301c334c0c2080d2a351
Gabriel Oliveira <gab.oliveira@outlook.com>
2013-06-24 22:16:28 -0300

    - Morrer implementado

--- src/tp/boundaries/entities/Player.java
+++ src/tp/boundaries/entities/Player.java
@@ -1,11 +1,16 @@
 package tp.boundaries.entities;
 
+import java.awt.event.WindowEvent;
+
+import javax.swing.JOptionPane;
+
 import tp.boundaries.Game;
 import tp.boundaries.InputHandler;
 import tp.boundaries.gfx.Colours;
 import tp.boundaries.gfx.Font;
 import tp.boundaries.gfx.Screen;
 import tp.boundaries.level.Level;
+import tp.boundaries.net.packets.Packet01Disconnect;
 import tp.boundaries.net.packets.Packet02Move;
 
 public class Player extends Mob {
@@ -15,6 +20,7 @@ public class Player extends Mob {
     private int scale = 1;
     protected boolean isSwimming = false;
     protected boolean isLavaing = false;
+    protected boolean isDead = false;
     private int tickCount = 0;
     private String username;
     private int pLevel;
@@ -67,86 +73,96 @@ public class Player extends Mob {
         if (isLavaing && level.getTile(this.x >> 3, this.y >> 3).getId() != 4) {
             isLavaing = false;
         }
+        
+        if(hp <= 0){
+        	isDead = true;
+        	if(Game.game.player == this) {
+	        	JOptionPane.showMessageDialog(Game.game.frame, "Você morreu :(");
+		        Game.game.frame.dispatchEvent(new WindowEvent(Game.game.frame, WindowEvent.WINDOW_CLOSING));
+        	}
+        }
+        
         tickCount++;
     }
 
     public void render(Screen screen) {
-        int xTile = 0;
-        int yTile = 28;
-        int walkingSpeed = 4;
-        int flipTop = (numSteps >> walkingSpeed) & 1;
-        int flipBottom = (numSteps >> walkingSpeed) & 1;
-
-        if (movingDir == 1) {
-            xTile += 2;
-        } else if (movingDir > 1) {
-            xTile += 4 + ((numSteps >> walkingSpeed) & 1) * 2;
-            flipTop = (movingDir - 1) % 2;
-        }
-
-        int modifier = 8 * scale;
-        int xOffset = x - modifier / 2;
-        int yOffset = y - modifier / 2 - 4;
-        
-        if (isSwimming) {
-            int waterColour = 0;
-            yOffset += 4;
-            if (tickCount % 60 < 15) {
-                waterColour = Colours.get(-1, -1, 225, -1);
-            } else if (15 <= tickCount % 60 && tickCount % 60 < 30) {
-                yOffset -= 1;
-                waterColour = Colours.get(-1, 225, 115, -1);
-            } else if (30 <= tickCount % 60 && tickCount % 60 < 45) {
-                waterColour = Colours.get(-1, 115, -1, 225);
-            } else {
-                yOffset -= 1;
-                waterColour = Colours.get(-1, 225, 115, -1);
-            }
-            screen.render(xOffset, yOffset + 3, 0 + 27 * 32, waterColour, 0x00, 1);
-            screen.render(xOffset + 8, yOffset + 3, 0 + 27 * 32, waterColour, 0x01, 1);
-        }
-        if (isLavaing) {
-            int lavaColour = 0;
-            yOffset += 4;
-            if (tickCount % 60 < 15) {
-                lavaColour = Colours.get(-1, -1, 496, -1);
-            } else if (15 <= tickCount % 60 && tickCount % 60 < 30) {
-                yOffset -= 1;
-                lavaColour = Colours.get(-1, 496, 161, -1);
-            } else if (30 <= tickCount % 60 && tickCount % 60 < 45) {
-                lavaColour = Colours.get(-1, 161, -1, 496);
-            } else {
-                yOffset -= 1;
-                lavaColour = Colours.get(-1, 496, 161, -1);
-            }
-            screen.render(xOffset, yOffset + 3, 0 + 27 * 32, lavaColour, 0x00, 1);
-            screen.render(xOffset + 8, yOffset + 3, 0 + 27 * 32, lavaColour, 0x01, 1);
-        }
-        
-        screen.render(xOffset + (modifier * flipTop), yOffset, xTile + yTile * 32, colour, flipTop, scale);
-        screen.render(xOffset + modifier - (modifier * flipTop), yOffset, (xTile + 1) + yTile * 32, colour, flipTop, scale);
-
-        if (!isSwimming && !isLavaing) {
-            screen.render(xOffset + (modifier * flipBottom), yOffset + modifier, xTile + (yTile + 1) * 32, colour,
-                    flipBottom, scale);
-            screen.render(xOffset + modifier - (modifier * flipBottom), yOffset + modifier, (xTile + 1) + (yTile + 1)
-                    * 32, colour, flipBottom, scale);
-        }
-        if (username != null) {
-            Font.render(username, screen, xOffset - ((username.length() - 1) / 2 * 8), yOffset - 10,
-                    Colours.get(-1, -1, -1, 555), 1);
-        }
-        
-        if (hp > 0) {
-            Font.render(Integer.toString(hp), screen, xOffset + 2 * username.length(), yOffset - 20,
-                    Colours.get(-1, -1, 215, 511), 1);
-        }
-        
-        if (pLevel != 0) {
-            Font.render("Lv." + Integer.toString(pLevel) + " ", screen, xOffset - 5 * username.length(), yOffset - 20,
-                    Colours.get(-1, -1, 215, 553), 1);
-        }
-        
+    	if(!isDead) {
+	        int xTile = 0;
+	        int yTile = 28;
+	        int walkingSpeed = 4;
+	        int flipTop = (numSteps >> walkingSpeed) & 1;
+	        int flipBottom = (numSteps >> walkingSpeed) & 1;
+	
+	        if (movingDir == 1) {
+	            xTile += 2;
+	        } else if (movingDir > 1) {
+	            xTile += 4 + ((numSteps >> walkingSpeed) & 1) * 2;
+	            flipTop = (movingDir - 1) % 2;
+	        }
+	
+	        int modifier = 8 * scale;
+	        int xOffset = x - modifier / 2;
+	        int yOffset = y - modifier / 2 - 4;
+	        
+	        if (isSwimming) {
+	            int waterColour = 0;
+	            yOffset += 4;
+	            if (tickCount % 60 < 15) {
+	                waterColour = Colours.get(-1, -1, 225, -1);
+	            } else if (15 <= tickCount % 60 && tickCount % 60 < 30) {
+	                yOffset -= 1;
+	                waterColour = Colours.get(-1, 225, 115, -1);
+	            } else if (30 <= tickCount % 60 && tickCount % 60 < 45) {
+	                waterColour = Colours.get(-1, 115, -1, 225);
+	            } else {
+	                yOffset -= 1;
+	                waterColour = Colours.get(-1, 225, 115, -1);
+	            }
+	            screen.render(xOffset, yOffset + 3, 0 + 27 * 32, waterColour, 0x00, 1);
+	            screen.render(xOffset + 8, yOffset + 3, 0 + 27 * 32, waterColour, 0x01, 1);
+	        }
+	        if (isLavaing) {
+	            int lavaColour = 0;
+	            yOffset += 4;
+	            if (tickCount % 60 < 15) {
+	                lavaColour = Colours.get(-1, -1, 496, -1);
+	            } else if (15 <= tickCount % 60 && tickCount % 60 < 30) {
+	                yOffset -= 1;
+	                lavaColour = Colours.get(-1, 496, 161, -1);
+	            } else if (30 <= tickCount % 60 && tickCount % 60 < 45) {
+	                lavaColour = Colours.get(-1, 161, -1, 496);
+	            } else {
+	                yOffset -= 1;
+	                lavaColour = Colours.get(-1, 496, 161, -1);
+	            }
+	            screen.render(xOffset, yOffset + 3, 0 + 27 * 32, lavaColour, 0x00, 1);
+	            screen.render(xOffset + 8, yOffset + 3, 0 + 27 * 32, lavaColour, 0x01, 1);
+	        }
+	        
+	        screen.render(xOffset + (modifier * flipTop), yOffset, xTile + yTile * 32, colour, flipTop, scale);
+	        screen.render(xOffset + modifier - (modifier * flipTop), yOffset, (xTile + 1) + yTile * 32, colour, flipTop, scale);
+	
+	        if (!isSwimming && !isLavaing) {
+	            screen.render(xOffset + (modifier * flipBottom), yOffset + modifier, xTile + (yTile + 1) * 32, colour,
+	                    flipBottom, scale);
+	            screen.render(xOffset + modifier - (modifier * flipBottom), yOffset + modifier, (xTile + 1) + (yTile + 1)
+	                    * 32, colour, flipBottom, scale);
+	        }
+	        if (username != null) {
+	            Font.render(username, screen, xOffset - ((username.length() - 1) / 2 * 8), yOffset - 10,
+	                    Colours.get(-1, -1, -1, 555), 1);
+	        }
+	        
+	        if (hp > 0) {
+	            Font.render(Integer.toString(hp), screen, xOffset + 2 * username.length(), yOffset - 20,
+	                    Colours.get(-1, -1, 215, 511), 1);
+	        }
+	        
+	        if (pLevel != 0) {
+	            Font.render("Lv." + Integer.toString(pLevel) + " ", screen, xOffset - 5 * username.length(), yOffset - 20,
+	                    Colours.get(-1, -1, 215, 553), 1);
+	        }
+    	}
     }
 
     public boolean hasCollided(int xa, int ya) {
